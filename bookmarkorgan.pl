9755fa16d4b531165cb5a47a5881ff7efc472389
Adamas hinton <giaeths@gmail.com>
2015-08-06 23:23:28 +1000

    add better interactive support

--- bookmarkorgan.pl
+++ bookmarkorgan.pl
@@ -164,17 +164,25 @@ TEST_PLUGIN:
         }
     }
     if ( not $plugin_fits ) {
-        $title       = prompt('title>');
-        $description = prompt('description>');
-        while ( my $tag_input = prompt( 'tag>', { -until => '' } ) ) {
-            push @{$tags_ref}, $tag_input;
+        if ( $enviroment{interactive_add} ) {
+            if ( not prompt( 'add? [Y/N]>', '-yes_no','-1' ) ) {
+                return;
+            }
+            $title       = prompt('title>');
+            #$description = prompt('description>');
+            while ( my $tag_input = prompt( 'tag>', { -until => $EMPTY } ) ) {
+                push @{$tags_ref}, $tag_input;
+            }
+            print $title,       "\n";
+            #print $description, "\n";
+            for ( @{$tags_ref} ) {
+                print qq["$_"\n];
+            }
+            if ( not prompt( 'is this correct [Y/N]>', '-yes_no' ) ) {
+                return;
+            }
         }
-        print $title,       "\n";
-        print $description, "\n";
-        for ( @{$tags_ref} ) {
-            print qq["$_"\n];
-        }
-        if ( not prompt( 'is this correct [Y/N]>', '-yes_no' ) ) {
+        else {
             return;
         }
     }
